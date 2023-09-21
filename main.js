61e7beda9d3136d2819c36c39024c0423c69ebab
vee/torio <134203950+veetorio@users.noreply.github.com>
2023-09-21 16:42:58 -0300

    :tada: Commit inicial

---
+++ main.js
@@ -0,0 +1,12 @@
+// get alll key
+const keys = document.querySelectorAll(".key")
+
+
+// play notes
+
+// click with Mouse
+
+// keyboard type
+window.addEventListener("keydown",function(event){
+    console.log(event)
+})
